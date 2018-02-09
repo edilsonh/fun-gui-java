70a85695ba488b10b48635f2aa20ade77b31f31c
Edilson Hernandez <edilson.hernandez1@gmail.com>
2018-02-09 17:02:04 -0500

    Add method to check days selected, output message based on selection

--- Fun.java
+++ Fun.java
@@ -15,15 +15,15 @@ public class Fun extends JFrame {
     JLabel q1 = new JLabel("Do you like fun?", JLabel.CENTER);
     JLabel q2 = new JLabel("When do you like to have fun?", JLabel.CENTER);
     ButtonGroup radio = new ButtonGroup();
-    JRadioButton  regular = new JRadioButton("Yes!", true);
-    JRadioButton  extra = new JRadioButton("A lot!!!", false);
+    JRadioButton regular = new JRadioButton("Yes!", true);
+    JRadioButton extra = new JRadioButton("A lot!!!", false);
     JButton result = new JButton("Let see what we get!");
     radio.add(regular);
     radio.add(extra);
 
-    Checkbox fri = new Checkbox("Friday");
-    Checkbox sat = new Checkbox("Saturday");
-    Checkbox sun = new Checkbox("Sunday");
+    JCheckBox fri = new JCheckBox("Friday");
+    JCheckBox sat = new JCheckBox("Saturday");
+    JCheckBox sun = new JCheckBox("Sunday");
 
     add(q1);
     firstPanel.add(regular);
@@ -42,9 +42,32 @@ public class Fun extends JFrame {
 
     result.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-        System.out.println("button pressed");
+
+        if (regular.isSelected()) {
+          JOptionPane.showMessageDialog(new JFrame(), "Likes to have a little bit of fun on:\n" + getDays(fri, sat, sun));
+        } else {
+          JOptionPane.showMessageDialog(new JFrame(), "Likes to have TONS of fun on:\n" + getDays(fri, sat, sun));
+        }
+
       }
     });
+  }
+
+  public static String getDays(JCheckBox f, JCheckBox st, JCheckBox sn){
+    String s = "";
+    if (f.isSelected()){
+      s+="Friday\n";
+    }
+    if (st.isSelected()) {
+      s+="Saturday\n";
+    }
+    if (sn.isSelected()) {
+      s+="Sunday\n";
+    }
+    if ((f.isSelected() == false) && (st.isSelected() == false) && (sn.isSelected() == false)) {
+      s+= "Not too often";
+    }
+    return s;
   }
 
   public static void main(String args[]){
